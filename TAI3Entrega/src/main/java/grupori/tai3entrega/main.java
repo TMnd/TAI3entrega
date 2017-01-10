1c2f24d9cb6e09c9da32244c67e236e3e2f3c381
João Amaral <amaral.joao@ua.pt>
2017-01-10 17:53:30 +0000

    Merge dos ficheiros .freqs e compressao

--- TAI3Entrega/src/main/java/grupori/tai3entrega/main.java
+++ TAI3Entrega/src/main/java/grupori/tai3entrega/main.java
@@ -5,47 +5,151 @@
  */
 package grupori.tai3entrega;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import SevenZip.Compression.LZMA.*;
 
-/**
- *
- * @author joaoa
- */
 public class main {
-    private static void freqsGenerator(String ficheiro){
+    private static int contadorFreqs = 0;
+    private static int contadorCorrerArrayFiles = 0;
+    
+    private static void freqsGenerator(String ficheiro) {
         String nomeFicheiro[] = ficheiro.split("\\.");
         try {
-            //String CMD = "\\GetMaxFreqs.exe " + musicaCaminho +".freq " + musicaCaminho +".wav";
-            String CMD ="cmd.exe /c cd src//main//java//grupori//tai3entrega && GetMaxFreqs.exe -w "+nomeFicheiro[0]+".freqs "+nomeFicheiro[0]+".wav";
-            // Run "script" Windows command
+            String CMD = "cmd.exe /c cd src//main//java//grupori//tai3entrega && GetMaxFreqs.exe -w " + nomeFicheiro[0] + ".freqs " + nomeFicheiro[0] + ".wav";
+            // Correr o "script" do cmd
             Process process = Runtime.getRuntime().exec(CMD);
+            process.waitFor();
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
     }
-
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        //System.out.println("Insira o caminho da pasta:");
-        String PastaMusicaCaminho = "src\\main\\java\\grupori\\tai3entrega";//sc.next();
+    
+    private static void gerarFreqs(String PastaMusicaCaminho){
+        File VerAPasta = new File(PastaMusicaCaminho);
+        File[] listOfFiles = VerAPasta.listFiles();
         
-        File folder = new File(PastaMusicaCaminho);
-        File[] listOfFiles = folder.listFiles();
-
         for (File file : listOfFiles) {
             if (file.isFile()) {
-                if(file.getName().endsWith(".wav")){
+                if (file.getName().endsWith(".wav")) {
                     freqsGenerator(file.getName());
+                    contadorFreqs++;
                 }
             }
         }
+    }
+
+    private static void mergeFiles(File[] files, File mergedFile) {
+        FileWriter fstream = null;
+        BufferedWriter out = null;
+        try {
+            fstream = new FileWriter(mergedFile, true);
+            out = new BufferedWriter(fstream);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        for (File f : files) {
+            FileInputStream fis;
+            try {
+                fis = new FileInputStream(f);
+                BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+                String aLine;
+                while ((aLine = in.readLine()) != null) {
+                    out.write(aLine);
+                    out.newLine();
+                }
+
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void juntarFicheiros(String PastaMusicaCaminho, String PathParaFichMerged) throws InterruptedException{
+        File VerAPasta = new File(PastaMusicaCaminho);
+        File[] listarFicheiros = VerAPasta.listFiles();
+        
+        File[] files = new File[contadorFreqs];
+
+        for (File file : listarFicheiros) {
+            if (file.isFile()) {
+                if (file.getName().endsWith(".freqs")) {
+                    files[contadorCorrerArrayFiles] = new File(file.getAbsolutePath());
+                    contadorCorrerArrayFiles++;
+                }
+            }
+        }
+        File mergedFile = new File(PathParaFichMerged);
+        mergeFiles(files, mergedFile);
+    }
+
+    private static void comprimir(String ficheiro) throws FileNotFoundException, IOException {
+        //Ler input para ser comprimido
+        File inputToCompress = new File(ficheiro);
+        BufferedInputStream inStream = new BufferedInputStream(new java.io.FileInputStream(inputToCompress));
+        //Criar o ficheiro 7zip
+        String nomeFicheiro[] = ficheiro.split("\\.");
+        File compressedOutput = new File(nomeFicheiro[0]+"_c.7z");
+        BufferedOutputStream outStream = new BufferedOutputStream(new java.io.FileOutputStream(compressedOutput));
+        // Criar p objecto encoder do LZMA / Header Information */
+        Encoder encoder = new Encoder();
+        encoder.SetAlgorithm(2);
+        encoder.SetDictionarySize(8388608);
+        encoder.SetNumFastBytes(128);
+        encoder.SetMatchFinder(1);
+        encoder.SetLcLpPb(3, 0, 2);
+        encoder.SetEndMarkerMode(false);
+        encoder.WriteCoderProperties(outStream);
+        long fileSize;
+        fileSize = inputToCompress.length();
+        for (int i = 0; i < 8; i++) {
+            outStream.write((int) (fileSize >>> (8 * i)) & 0xFF);
+        }
+        // Escrever dados compressados para ficheiro
+        encoder.Code(inStream, outStream, -1, -1, null);
+      
+        outStream.flush();
+        outStream.close();
+        inStream.close();
+    }
+    
+    private static void criariFicheirosComp(String PastaMusicaCaminho) throws IOException{
+        File VerAPasta = new File(PastaMusicaCaminho);
+        File[] listOfFiles = VerAPasta.listFiles();
+        
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                if (file.getName().endsWith(".freqs")) {
+                    comprimir(file.getPath());
+                }
+            }
+        }
+        
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Insira o caminho da pasta:");
+        String PastaMusicaCaminho = "src\\main\\java\\grupori\\tai3entrega";//sc.next();
+        String PathParaFichMerged = "src\\main\\java\\grupori\\tai3entrega\\merge.freqs"; //Caso queiram meter noutro sitio
+
+        System.out.println("A correr o programa do prof");
+        gerarFreqs(PastaMusicaCaminho);
+                
+        System.out.println("Merge dos ficheiros .freqs");
+        juntarFicheiros(PastaMusicaCaminho, PathParaFichMerged);
+                   
+        System.out.println("A comprimir os ficheiros:");
+        criariFicheirosComp(PastaMusicaCaminho);
+       
         System.out.println("Done");
     }
 }
